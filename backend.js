21e4b6c940c1c0aaabfac4f577a52efd684c6fd7
Shaun Fidler <shaunfidler@ufl.edu>
2021-02-19 12:42:53 -0500

    Added login route with print

--- backend.js
+++ backend.js
@@ -17,23 +17,37 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/register.html'));
+})
+
+app.post('/adduser', (req, res) => {
+    res.sendFile(path.join(__dirname + '/register.html'));
 })
 
 app.get('/loggedin', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/loggedin.html'));
 })
 
 app.get('/error', (req, res) => {
+    res.sendFile(path.join(__dirname + '/error.html'));
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    //console.log(req.body);
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
