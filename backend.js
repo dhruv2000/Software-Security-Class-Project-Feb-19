8fc71648c9caee9d3e7d4843c3ce2c4b996f4c54
Shaun Fidler <shaunfidler@ufl.edu>
2021-02-19 12:19:44 -0500

    Express app init

--- backend.js
+++ backend.js
@@ -1,18 +1,34 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+// //Load HTTP module
+// const http = require("http");
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+// //Create HTTP server and listen on port 3000 for requests
+// const server = http.createServer((req, res) => {
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+//   //Set the response HTTP header with HTTP status and Content type
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// //listen for request on port 3000, and as a callback function have the port listened on logged
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('You are home!')
+  })
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
